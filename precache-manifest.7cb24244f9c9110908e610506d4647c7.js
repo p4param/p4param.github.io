c3334f8a37e78f2b3e6ed25821310e91da9e0fca
paramjeet.singh <paramjeet.singh@exp-inc.com>
2019-08-20 14:18:53 +0530

    Updates

---
+++ precache-manifest.7cb24244f9c9110908e610506d4647c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7a7f1b79c39d447c34e08fd93e11169",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81b0d27cc2e4d7722eab",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c14e484f516540b6a51c",
+    "url": "/static/js/2.322fa7d7.chunk.js"
+  },
+  {
+    "revision": "81b0d27cc2e4d7722eab",
+    "url": "/static/js/main.9dfe522f.chunk.js"
+  },
+  {
+    "revision": "67da955d5f5914ba84a9",
+    "url": "/static/js/runtime~main.48de0d7a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
